c7691c60f8fb52a9c2062816c7cfbec4041a7ff8
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2020-12-03 09:04:53 +1000

    example of spread operator

--- javascript-algo-and-data-struc/es6/basics.js
+++ javascript-algo-and-data-struc/es6/basics.js
@@ -1,6 +1,17 @@
+// uses rest parameter
 const sum = (...args) => {
   return args.reduce((a, b) => a + b, 0);
 }
 console.log(sum(1,2,3)); // returns 6
 // The rest parameter eliminates the need to check the args array and allows us to apply map(), filter() and reduce() on the parameters array
+
+// uses spread operator
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // creates a duplicate of arr1
+
+arr1[1] = 2;
+console.log(arr2); // ['JAN', 'FEB', 'MAR', 'APR', 'MAY']
+console.log(arr1); //  ['JAN', 2, 'MAR', 'APR', 'MAY']
 
