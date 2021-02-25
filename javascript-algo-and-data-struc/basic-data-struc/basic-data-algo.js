2975c52a208db124a85558f5d7615ec3b3191cb8
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-02-26 08:18:44 +1000

    examples for data struc

--- javascript-algo-and-data-struc/basic-data-struc/basic-data-algo.js
+++ javascript-algo-and-data-struc/basic-data-struc/basic-data-algo.js
@@ -16,3 +16,16 @@ function forecast(arr) {
 
 // prints 'warm' and 'sunny'
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // Only change code below this line
+    newArr.push([...arr]);
+    // Only change code above this line
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
