1412d908bc0fca0c3ed3c67e6fd101a167ad21e3
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-02-26 12:52:18 +1000

    additional examples for objects

--- javascript-algo-and-data-struc/basic-data-struc/basic-data-algo.js
+++ javascript-algo-and-data-struc/basic-data-struc/basic-data-algo.js
@@ -97,3 +97,50 @@ let myNestedArray = [
   ]
   // Only change code above this line
 ];
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// Only change code above this line
+
+console.log(foods);
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(obj) {
+  // Only change code below this line
+  if ('Alan' in users && users.hasOwnProperty('Jeff') && 'Sarah' in users && 'Ryan' in users) {
+    return true;
+  }
+  return false;
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
