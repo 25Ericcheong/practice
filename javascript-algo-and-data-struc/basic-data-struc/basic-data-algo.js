351887b1007b40cf0986b6b8ec3d4c58922bfd16
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-02-26 12:00:39 +1000

    additional examples

--- javascript-algo-and-data-struc/basic-data-struc/basic-data-algo.js
+++ javascript-algo-and-data-struc/basic-data-struc/basic-data-algo.js
@@ -51,3 +51,28 @@ function quickCheck(arr, elem) {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// will remove arrays within nested array if a particular elem exists in the array within the nested array
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // Only change code below this line
+  for (let i = 0; i < arr.length; i++) {
+
+    let temp = arr[i]
+    for (let j = 0; j < temp.length; j++) {
+      console.log(temp[j]);
+      if (temp[j] == elem) {
+        temp = [];
+        break;
+      }
+    }
+
+    if (temp.length != 0) {
+      newArr.push([...temp])
+    }
+  }
+  // Only change code above this line
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
