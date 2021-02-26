f0f77a1af969a238b16ae1f644dacb581335b5e2
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-02-27 09:04:49 +1000

    additional algo practice examples

--- javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
+++ javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
@@ -53,3 +53,42 @@ function findLongestWordLength(str) {
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
+// largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27, 5, 39, 1001].
+function largestOfFour(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let tempSubArray = arr[i];
+
+    let tempNum = arr[i][0];
+    for (let j = 1; j < tempSubArray.length; j++) {
+      if (tempSubArray[j] > tempNum) {
+        tempNum = tempSubArray[j];
+      }
+    }
+    arr[i] = tempNum;
+  }
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") should return false
+function confirmEnding(str, target) {
+  let words = str.split(" ");
+  let targetLen = target.length - 1;
+
+  let word = words[words.length - 1];
+  let index = word.length - 1;
+  for (let i = targetLen; i >= 0; i--) {
+
+    if (word.charAt(index) != target.charAt(i)) {
+      console.log("enter");
+      return false;
+    }
+
+    index--;
+  }
+  return true;
+}
+
+confirmEnding("Bastian", "n");
+
