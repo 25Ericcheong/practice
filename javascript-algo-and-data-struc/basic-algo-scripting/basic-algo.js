7a3123ef94b0c0e5802da8486148fcdf62c617f8
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-02-27 10:05:42 +1000

    more examples from challenges

--- javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
+++ javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
@@ -92,3 +92,77 @@ function confirmEnding(str, target) {
 
 confirmEnding("Bastian", "n");
 
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+function repeatStringNumTimes(str, num) {
+  let store = "";
+  for (let i = 0; i < num; i++) {
+    store += str;
+  }
+  return store;
+}
+
+repeatStringNumTimes("abc", 3);
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+function truncateString(str, num) {
+  let words = str.split(" ");
+  let countStr = words.length - 1;
+  let store = "";
+
+  for (let i = 0; i < words.length; i++) {
+    countStr += words[i].length;
+  }
+
+  if (countStr > num) {
+    for (let i = 0; i < num; i++) {
+      store += str.charAt(i);
+    }
+    return store + "..."
+  } else {
+    return str;
+  }
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+function findElement(arr, func) {
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (func(arr[i])) {
+      return arr[i]
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+// Check if a value is classified as a boolean primitive. Return true or false.
+function booWho(bool) {
+  if (bool === true || bool === false) {
+    return true;
+  }
+  return false;
+}
+
+booWho(null);
+
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+function titleCase(str) {
+  let words = str.split(" ");
+  let store = "";
+
+  for (let i = 0; i < words.length; i++) {
+    let lowerWord = words[i].toLowerCase();
+    let otherHalf = lowerWord.slice(1, lowerWord.length)
+
+    store += lowerWord.charAt(0).toUpperCase() + otherHalf + " ";
+  }
+  return store.slice(0,store.length - 1)
+}
+
+titleCase("I'm a little tea pot");
+
