5c98746084b7256512db4c6c6cd5a1d68462daa9
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-03-08 09:26:38 +1000

    additional practice questions

--- javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
+++ javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
@@ -192,3 +192,62 @@ function loopAddArr(arr1, arr2) {
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+function bouncer(arr) {
+  let store = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      store.push(arr[i]);
+    }
+  }
+  return store;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+function getIndexToIns(arr, num) {
+  
+  // bubble sort
+  for (let lastIndex = arr.length - 1; lastIndex > 0; lastIndex--) {
+    for (let i = 0; i < lastIndex; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+      }
+    }
+  }
+
+  let track = 0;
+  for (let i = 0; i < arr.length; i++) {
+    track = i;
+    if (arr[i] >= num) {
+      return i;
+    }
+  }
+
+  if (arr[track] <= num) {
+    return track + 1;
+  }
+
+  return 0;
+}
+
+function swap(arr, i, j) {
+  if (i == j) {
+    return;
+  }
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+getIndexToIns([40, 60], 50);
