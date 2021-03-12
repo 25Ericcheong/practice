7a0b0a507afbead7f9e41cae5c13d209b5c1e383
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-03-13 09:34:42 +1000

    tools for javascript and created folders for another section of module

--- javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
+++ javascript-algo-and-data-struc/basic-algo-scripting/basic-algo.js
@@ -274,6 +274,9 @@ function mutation(arr) {
 mutation(["hello", "hey"]);
 
 // Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+// chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+
 function chunkArrayInGroups(arr, size) {
   let mainArr = [];
   let numMiniArr = Math.ceil(arr.length / size);
