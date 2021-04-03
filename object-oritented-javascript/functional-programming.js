fc42232b496869d1ac70f942afa88070418d9199
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-03 20:55:40 +1000

    more oop practice

--- object-oritented-javascript/functional-programming.js
+++ object-oritented-javascript/functional-programming.js
@@ -68,7 +68,7 @@ console.log(
 );
 
 
-
+// Understand the Hazards of Using Imperative Code
 // tabs is an array of titles of each site open within the window
 var Window = function(tabs) {
   this.tabs = tabs; // We keep a record of the array inside the object
@@ -114,3 +114,42 @@ var finalTabs = socialWindow
   .join(videoWindow.tabClose(2)) // Close third tab in video window, and join
   .join(workWindow.tabClose(1).tabOpen());
 console.log(finalTabs.tabs);
+
+// So far, we have seen two distinct principles for functional programming:
+
+// Don't alter a variable or object - create new variables and objects and return them if need be from a function. Hint: using something like var newArr = arrVar, where arrVar is an array will simply create a reference to the existing variable and not a copy. So changing a value in newArr would change the value in arrVar.
+// Declare function parameters - any computation inside a function depends only on the arguments passed to the function, and not on any global object or variable.
+// The global variable
+// Rewrite the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of the array passed to it and return a new array (list). The remove function should remove the given bookName from the array passed to it.
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add (books, bookName) {
+  // Creates a copy of old array without impacting old array
+  let newArray = [...books];
+  newArray.push(bookName);
+  return newArray;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove (books, bookName) {
+  let newArray = [...books];
+  var book_index = newArray.indexOf(bookName);
+  if (book_index >= 0) {
+    newArray.splice(book_index, 1);
+    return newArray;
+
+    // Change code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
+
+
+
