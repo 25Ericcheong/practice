80a21059bcf6769bcbc7ef10b56c1703981548be
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-09 09:39:00 +1000

    more functional programming practices

--- object-oritented-javascript/functional-programming.js
+++ object-oritented-javascript/functional-programming.js
@@ -463,3 +463,47 @@ function splitify(str) {
   // Only change code above this line
 }
 splitify("Hello World,I-am code");
+
+
+
+// Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars. For this challenge, do not use the replace method.
+function sentensify(str) {
+  // Only change code below this line
+  let arrStr = str.split(/\W+/);
+  return arrStr.join(" ");
+  // Only change code above this line
+}
+sentensify("May-the-force-be-with-you");
+
+
+
+// Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+// The input is a string with spaces and title-cased words
+// The output is a string with the spaces between words replaced by a hyphen (-)
+// The output should be all lower-cased letters
+// The output should not have any spaces
+// Only change code below this line
+function urlSlug(title) {
+  let arrStr = (title.toLowerCase()).split(/\W+/);
+
+  // first word can be an empty space so need to get rid of it
+  if (arrStr[0] == "") {
+    arrStr.shift();
+  }
+  return arrStr.join("-");
+}
+// Only change code above this line
+console.log(urlSlug(" Winter Is  Coming"));
+
+
+
+// Use the every method inside the checkPositive function to check if every element in arr is positive. The function should return a Boolean value.
+function checkPositive(arr) {
+  // Only change code below this line
+  return arr.every(function(currentVal) {
+    return currentVal > 0;
+  });
+
+  // Only change code above this line
+}
+console.log(checkPositive([1, 2, 3, -4, 5]));
