c4719262b25b8e85a927f2f37a7dec3dbf0f3e91
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-12 08:38:35 +1000

    more practice

--- object-oritented-javascript/intermediate-algo-script.js
+++ object-oritented-javascript/intermediate-algo-script.js
@@ -82,3 +82,31 @@ function destroyer(arr) {
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 
 
+
+// Wherefore art thou
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+function whatIsInAName(collection, source) {
+  var arr = [];
+  let sourceLen = Object.keys(source).length;
+  for (let i = 0; i < collection.length; i++) {
+    let item = collection[i];
+    let track = 0;
+
+    for (let key in item) {
+      let value = item[key];
+
+      if (source.hasOwnProperty(key) && source[key] == value) {
+        console.log(track);
+        track++;
+      }
+    }
+
+    if (track == sourceLen) {
+      arr.push(item);
+    }
+  }
+
+  return arr;
+}
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 })
+// whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }) should return [{ "apple": 1, "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }].
