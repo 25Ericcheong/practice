8982815c563aaf26fcdaba79b7f4f0a558d29ed3
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-18 13:38:34 +1000

    drop elements challenge

--- object-oritented-javascript/intermediate-algo-script.js
+++ object-oritented-javascript/intermediate-algo-script.js
@@ -403,3 +403,21 @@ function sortNumber(a, b) {
 }
 
 smallestCommons([5,1]);
+
+
+
+// Drop it
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array
+
+function dropElements(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i]) == true) {
+      return arr.slice(i, arr.length);
+    }
+  }
+  return [];
+}
+
+dropElements([1, 2, 3], function(n) {return n < 3; });
