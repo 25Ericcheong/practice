74235e5110cf45a940cdc29e07b16b1b47ca7269
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-21 09:19:56 +1000

    truthy challenge

--- object-oritented-javascript/intermediate-algo-script.js
+++ object-oritented-javascript/intermediate-algo-script.js
@@ -468,3 +468,21 @@ binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 0110
 
 
 
+// Everything Be True
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+// In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+// In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+// Remember, you can access object properties through either dot notation or [] notation.
+function truthCheck(collection, pre) {
+  for (let key in collection) {
+    if (!collection[key][pre]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
