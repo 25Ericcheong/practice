1d445e59ac49109896e0dd18363513756817e05a
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-22 20:52:33 +1000

    interesting challenge to do - optional arguments

--- object-oritented-javascript/intermediate-algo-script.js
+++ object-oritented-javascript/intermediate-algo-script.js
@@ -486,3 +486,51 @@ function truthCheck(collection, pre) {
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+
+// Arguments Optional
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+// For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+
+// Calling this returned function with a single argument will then return the sum:
+
+// var sumTwoAnd = addTogether(2);
+// sumTwoAnd(3) returns 5.
+
+// If either argument isn't a valid number, return undefined
+function addTogether() {
+  
+  if (arguments.length == 2) {
+    let firstArg = arguments[0];
+    let secondArg = arguments[1];
+
+    if (Number.isInteger(firstArg) && Number.isInteger(secondArg)) {
+      return firstArg + secondArg;
+    } else {
+      return undefined;
+    }
+
+  } else if (arguments.length == 1) {
+    let firstArg = arguments[0];
+
+    if (Number.isInteger(firstArg)) {
+
+      function forSecondArg() {
+        if (Number.isInteger(arguments[0])) {
+          return firstArg + arguments[0];
+        } else {
+          return undefined;
+        }
+      }
+
+      return forSecondArg;
+    } else {
+      return undefined;
+    }
+  }
+}
+
+addTogether(23, 30)
+console.log(addTogether(5)(7));
