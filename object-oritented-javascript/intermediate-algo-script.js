eb51b11ec41fb0427318f7d06ef81d3699e5b4eb
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-24 13:18:14 +1000

    last challenge for intermediate challenge

--- object-oritented-javascript/intermediate-algo-script.js
+++ object-oritented-javascript/intermediate-algo-script.js
@@ -578,3 +578,42 @@ var Person = function(firstAndLast) {
 
 var bob = new Person('Bob Ross');
 bob.getFullName();
+
+
+
+// Map the Debris
+// Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+// The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+// You can read about orbital periods on Wikipedia.
+
+// The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+// The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.
+function orbitalPeriod(arr) {
+  let result = [];
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+
+  for (let index in arr) {
+     let store = {};
+     
+    for (let key in arr[index]) {
+      let val = arr[index][key];
+
+      if (typeof val === "string") {
+        store['name'] = val;
+      } else {
+        var orbitalRadius = earthRadius + val;
+        const constant = 2 * Math.PI;
+        let orbitalPeriod = Math.round(constant * (Math.sqrt(Math.pow(orbitalRadius,3) / GM)));
+        store['orbitalPeriod'] = orbitalPeriod;
+      }
+    }
+    result.push(store);
+  }
+  return result;
+}
+
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
