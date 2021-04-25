da81dc63f14b041d04fc5c6f194e2068d54db2bb
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-04-25 10:14:33 +1000

    palindrome challenge

---
+++ object-oritented-javascript/projects.js
@@ -0,0 +1,32 @@
+// Palindrome Checker
+// Return true if the given string is a palindrome. Otherwise, return false.
+
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+// Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+// We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+// We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+function palindrome(str) {
+  let lowerStr = str.toLowerCase();
+
+  // remove all non alphanumeric
+  lowerStr = lowerStr.replace(/[^0-9a-z]/gi, '')
+
+  var len = Math.floor(lowerStr.length / 2);
+  for (var i = 0; i < len; i++)
+    if (lowerStr[i] !== lowerStr[lowerStr.length - i - 1]) {
+      return false;
+    }
+  return true;
+
+  // or a one liner solution & shortcut
+  // return str == str.split('').reverse().join('');
+
+}
+
+palindrome("0_0 (: /-\ :) 0-0");
+
+
+
