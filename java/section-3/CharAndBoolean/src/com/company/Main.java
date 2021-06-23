53ff537fcef11a7786d2d4ac55b13cbb322bad54
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-06-23 17:33:55 +1000

    primitive data types

---
+++ java/section-3/CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
--- java/section-3/FloatAndDouble/src/com/company/Main.java
+++ java/section-3/FloatAndDouble/src/com/company/Main.java
@@ -14,10 +14,20 @@ public class Main {
         System.out.println("Double min : " + myMinDoubleValue);
         System.out.println("Double max : " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5d;
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+        System.out.println("Int val : " + myIntValue);
+        System.out.println("Float val : " + myFloatValue);
+        System.out.println("Double val : " + myDoubleValue);
 
-        
+        double numberOfPounds = 200d;
+        double convertKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms = " + convertKilograms);
+
+        double pi = 3.1415927d;
+        // to make it more human readable perhaps
+        double anotherNumber = 3_000_000.4_567;
+        System.out.println(anotherNumber);
     }
 }
