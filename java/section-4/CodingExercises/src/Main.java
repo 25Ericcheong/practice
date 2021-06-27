017ea5c5d1a07663f5673113f7120e0c3925dfc8
Eric Cheong Kai Kit <ericcheongkaikit@gmail.com>
2021-06-28 09:07:17 +1000

    java exercises

---
+++ java/section-4/CodingExercises/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Coding exercise 1
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+
+
+        // Coding exercise 2
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+    }
+}
