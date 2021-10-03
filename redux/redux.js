f127e28510ec4e657ef0736906da4a5671ccba32
25Ericcheong <ericcheongkaikit@gmail.com>
2021-10-03 13:03:37 +1000

    removing an item from state without mutating state

--- redux/redux.js
+++ redux/redux.js
@@ -422,3 +422,27 @@ const addToDo = (todo) => {
 }
 
 const store = Redux.createStore(immutableReducer);
+
+
+
+// Remove an Item from an Array
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
+  switch(action.type) {
+    case 'REMOVE_ITEM':
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1, state.length)
+      ];
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: 'REMOVE_ITEM',
+    index
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
