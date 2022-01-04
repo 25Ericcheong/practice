3e30acd229353393b8a1be1c69a3b9c8a5652a88
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-05 08:11:39 +1000

    a little advance calculator

--- c-programming/tutorial/calculator.c
+++ c-programming/tutorial/calculator.c
@@ -5,12 +5,30 @@ int main()
 {
   double num1;
   double num2;
+  char op;
+
   printf("Enter first number: ");
   scanf("%lf", &num1);
+
+  printf("Enter operator: ");
+  scanf(" %c", &op);
 
   printf("Enter second number: ");
   scanf("%lf", &num2);
 
-  printf("Answer: %f", num1 + num2);
+  double result;
+  if (op == '+') {
+    result = num1 + num2;
+  } else if (op == '-') {
+    result = num1 - num2;
+  } else if (op == '/') {
+    result = num1 / num2;
+  } else if (op == '*') {
+    result = num1 * num2;
+  } else {
+    printf("Invalid operator \n");2
+  }
+
+  printf("Answer: %f", result);
   return 0;
 }
