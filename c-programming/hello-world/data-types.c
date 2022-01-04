2f1788da777a038228957130ac5f309f96af8605
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-04 13:39:18 +1000

    basic data types

---
+++ c-programming/hello-world/data-types.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+int main()
+{
+  int age = 40;
+  double gpa = 3.7;
+  char grade = 'A';
+  char phrase[] = "A String"; // cannot be modified
+
+  return 0;
+}
