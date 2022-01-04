3a977a871ff80eb808666f74b95d387cbacf31e6
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-04 17:57:26 +1000

    return functions

--- c-programming/tutorial/function.c
+++ c-programming/tutorial/function.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double cube(double num) {
+  double result = num * num * num;
+  return result;
+}
+
 int main() 
 {
+  printf("Answer: %f \n", cube(3.0));
+
   sayHi("Mike", 22);
   return 0;
 }
