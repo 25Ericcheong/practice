e3f5a6ab611de9c24b52190e9398a64fec8ba385
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-04 13:50:34 +1000

    working with numbers

--- c-programming/tutorial/printf.c
+++ c-programming/tutorial/printf.c
@@ -4,5 +4,11 @@
 int main() 
 {
   printf("Hello\"World\n");
-  printf("My favorite %s is %d or %f or %c?", "number", 500, 500.01, 'c');
+  printf("My favorite %s is %d or %f or %c?\n", "number", 500, 500.01, 'c');
+
+  int num = 6;
+  printf("%f \n \n", num);
+
+  printf("%f \n", pow(2, 3));
+  printf("%f", ceil(37.454));
 }
