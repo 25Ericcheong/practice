1638a67a148e60267e149c83d202f1e73139c94d
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-05 20:53:42 +1000

    reading an existing file

--- c-programming/tutorial/write-file.c
+++ c-programming/tutorial/write-file.c
@@ -7,9 +7,15 @@ int main()
   
   // fprintf(fpointer, "Jim, Salesman\nPam, Receptionist\nOscar, Accounting");
 
-  FILE * fpointer = fopen("employees.txt", "a");
+  // FILE * fpointer = fopen("employees.txt", "a");
   
-  fprintf(fpointer, "\nKelly, Customer Service");
+  // fprintf(fpointer, "\nKelly, Customer Service");
+
+  char line[255];
+  FILE * fpointer = fopen("employees.txt", "r");
+
+  fgets(line, 255, fpointer);
+  printf("%s", line);
 
   fclose(fpointer);
 
