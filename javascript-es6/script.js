be377a9355b50a1bf0f8ed271847d4b3ca5f200b
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-07 16:03:06 +1000

    better ellaboration on comments

--- javascript-es6/script.js
+++ javascript-es6/script.js
@@ -41,20 +41,23 @@ Object.setPrototypeOf(person, PersonPrototype);
 
 console.log(person.getName());
 
-// borrowing a function - have object borrow a function without objecti mplementing it
+// borrowing a function - have object borrow a function without object implementing it
 function sayHello() { console.log(`Greetings ${this.name}`) };
 var a = {name: 'peter'};
 
 // done by using call and apply available on Function.prototype
 // every function created inherits from Function.prototype
+// this allows a object to 'borrow' sayHellow without the need to implement or have it anywhere on its prototype chain
 sayHello.call(a);
 
 // upon understanding prototypal inheritance and borrowing functions - can understand why one should use hasOwnProperty on Object.prototype and not on object instance
+// this modifies the chain prototype
 var a = Object.create(null); // chain is now a -> null
 a.name = 'peter';
 
 // this causes error because (based on 3.7 of https://awesomeopensource.com/project/airbnb/javascript#objects)
 // object can be shadowed by properties on the object or object may be a null object as shown previously
+// this will now cause an error and will break code if we use object instance since prototype chain has been modified to be null
 // a.hasOwnProperty('name');
 
 // as such, best practice is to do this
@@ -62,7 +65,7 @@ const has = Object.prototype.hasOwnProperty;
 console.log(has.call(a, 'name'));
 
 
-// prefer object spread syntax over Object.assign to shallow-copy objects. Use object rest parameter syntax to get a new object with certain properties omitted
+// prefer object spread syntax over Object.assign to shallow-copy objects. Use object rest parameter syntax to get a new object with certain properties omitted (based on 3.8 of https://awesomeopensource.com/project/airbnb/javascript#objects)
 const original = { f: 1, b: 2 };
 const copy = { ...original, c: 3 }; // copy => { f: 1, b: 2, c: 3 }
 const { f, ...noF } = copy;
