4c8ed80a3bbe2f60e5c8e10a74754509d26a3753
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-18 13:16:14 +1000

    ensuring each component is unique with key and using spread operator

--- react-tutorial/1-component/app/src/index.js
+++ react-tutorial/1-component/app/src/index.js
@@ -5,16 +5,19 @@ import "./index.css";
 
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
     title: "Atomic Habits",
     author: "James Clear",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/81V8R5aqDYL._AC_UL200_SR200,200_.jpg",
     title: "American Marxism",
     author: "Mark R. Levin",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
     title: "If Animals Kissed Good Night",
     author: "Ann Whitford Paul",
@@ -25,14 +28,17 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props.book;
+  // destructuring object
+  // props.book because we sent book object previously
+  // can also directly send img, title, author and not book object but will change from props.book to props instead then
+  const { img, title, author } = props;
 
   return (
     <article className="book">
