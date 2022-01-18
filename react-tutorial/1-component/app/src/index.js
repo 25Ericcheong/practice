e72f1b81218ea45f80e7607f350bf01819c65151
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-18 11:56:59 +1000

    in line styling in jsx

--- react-tutorial/1-component/app/src/index.js
+++ react-tutorial/1-component/app/src/index.js
@@ -34,6 +34,10 @@ const Image = () => (
 );
 
 const Title = () => <h1>Atomic Habits</h1>;
-const Author = () => <h4>James Clear</h4>;
+const Author = () => (
+  <h4 style={{ color: "#617d98", fontSize: "0.75em", marginTop: "0.25rem" }}>
+    James Clear
+  </h4>
+);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
