419312df25e485f168885078b02020586107491f
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-19 08:42:02 +1000

    import export and segregated files

--- react-tutorial/1-component/app/src/index.js
+++ react-tutorial/1-component/app/src/index.js
@@ -3,26 +3,8 @@ import ReactDom from "react-dom";
 
 import "./index.css";
 
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
-    title: "Atomic Habits",
-    author: "James Clear",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81V8R5aqDYL._AC_UL200_SR200,200_.jpg",
-    title: "American Marxism",
-    author: "Mark R. Levin",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
-    title: "If Animals Kissed Good Night",
-    author: "Ann Whitford Paul",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -33,35 +15,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // attribute and event handler
-  const clickHandler = () => {
-    alert("Hello World");
-  };
-
-  const complexExample = (author) => {
-    console.log(author);
-  };
-
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        Examples
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        More complex
-      </button>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
