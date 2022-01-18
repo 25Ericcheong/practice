1ba39ab9fe7225d90ee3cae8831b3aad529dfd33
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-18 13:02:07 +1000

    mapping over simple list

--- react-tutorial/1-component/app/src/index.js
+++ react-tutorial/1-component/app/src/index.js
@@ -3,40 +3,26 @@ import ReactDom from "react-dom";
 
 import "./index.css";
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
-  title: "Atomic Habits",
-  author: "James Clear",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/91lk9FG9xQL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81V8R5aqDYL._AC_UL200_SR200,200_.jpg",
+    title: "American Marxism",
+    author: "Mark R. Levin",
+  },
+];
 
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81V8R5aqDYL._AC_UL200_SR200,200_.jpg",
-  title: "American Marxism",
-  author: "Mark R. Levin",
-};
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At quae
-          quasi eum totam recusandae iure. Ex dicta tempora, tenetur nam illum
-          consectetur accusantium inventore at consequuntur et odit consequatur
-          debitis?
-        </p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 }
 
 const Book = (props) => {
@@ -47,7 +33,6 @@ const Book = (props) => {
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
