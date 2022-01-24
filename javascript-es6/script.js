7b7260219c923b92ac925a9717528469253b4cc7
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-24 20:48:07 +1000

    leave blank line after blocks and before next statement

--- javascript-es6/script.js
+++ javascript-es6/script.js
@@ -808,3 +808,58 @@ const svg = leds.enter().append('svg:svg');
 svg.classed('led', true).attr('width', (radius + margin) * 2);
 const g = svg.append('svg:g');
 g.attr('transform', `translate(${radius + margin},${radius + margin})`).call(tron.led);
+
+
+// leave lbank line after blocks and before next statement
+// bad
+if (foo) {
+  return bar;
+}
+return baz;
+
+// good
+if (foo) {
+  return bar;
+}
+
+return baz;
+
+// bad
+const obj = {
+  foo() {
+  },
+  bar() {
+  },
+};
+return obj;
+
+// good
+const obj = {
+  foo() {
+  },
+
+  bar() {
+  },
+};
+
+return obj;
+
+// bad
+const arr = [
+  function foo() {
+  },
+  function bar() {
+  },
+];
+return arr;
+
+// good
+const arr = [
+  function foo() {
+  },
+
+  function bar() {
+  },
+];
+
+return arr;
