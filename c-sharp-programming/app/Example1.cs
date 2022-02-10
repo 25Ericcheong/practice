8f297476f63337befbff76732e30392bdf61e4c9
25Ericcheong <ericcheongkaikit@gmail.com>
2022-02-10 16:16:29 +1000

    example of typed parameters

---
+++ c-sharp-programming/app/Example1.cs
@@ -0,0 +1,10 @@
+namespace Example1;
+
+public class Pair<TFirst, TSecond>
+{
+  public TFirst First { get; }
+  public TSecond Second { get; }
+
+  public Pair(TFirst first, TSecond second) =>
+      (First, Second) = (first, second);
+}
